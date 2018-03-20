5e76998f20bc5e28a7e8d1557f63ec19387b307e
Ozbej Golob <ozbej.golob@gmail.com>
2018-03-20 10:24:03 +0100

    scaffold- update (dela)

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -79,7 +79,7 @@ class PlacesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def place_params
-      params.require(:place).permit(:name, :description, :picture)
+      params.require(:place).permit(:ime, :opis, :lokacija, :kraj)
     end
 end
 
