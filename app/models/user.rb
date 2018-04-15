1173323a32068f7027024179981ad15f06474bdb
Ozbej Golob <ozbej.golob@gmail.com>
2018-04-15 21:53:53 +0200

    google login- update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,21 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+has_many :place, dependent: :destroy
+has_many :comments
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  devise :omniauthable, omniauth_providers: [:google_oauth2]
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
 
   def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(email: data['email']).first
 
     # Uncomment the section below if you want users to be created if they don't exist
-    # unless user
-    #     user = User.create(name: data['name'],
-    #        email: data['email'],
-    #        password: Devise.friendly_token[0,20]
-    #     )
-    # end
-    user
+    unless user
+         user = User.create(email: data['email'],
+            password: Devise.friendly_token[0,20]
+         )
+     end
   end
-
-         has_many :place, dependent: :destroy
-         has_many :comments
-
 end
 
