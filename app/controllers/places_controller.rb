6fe7d7f6328065bae6358deb4bab8889bb305d7f
Ozbej Golob <ozbej.golob@gmail.com>
2018-04-17 08:32:43 +0200

    heroku dela

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -74,7 +74,7 @@ class PlacesController < ApplicationController
   def destroy
     @place.destroy
     respond_to do |format|
-      format.html { redirect_to place_url, notice: 'Place was successfully destroyed.' }
+      format.html { redirect_to root, notice: 'Place was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
