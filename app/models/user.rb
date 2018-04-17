cbf2e01f59e1b68e558edc742fb3c5098e57f02f
Ozbej Golob <ozbej.golob@gmail.com>
2018-04-17 08:49:31 +0200

    heroku change

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,11 @@ has_many :comments
     user = User.where(email: data['email']).first
 
     # Uncomment the section below if you want users to be created if they don't exist
-    unless user
-         user = User.create(email: data['email'],
-            password: Devise.friendly_token[0,20]
-         )
-     end
+    #unless user
+        # user = User.create(email: data['email'],
+           # password: Devise.friendly_token[0,20]
+        # )
+    # end
   end
 end
 
